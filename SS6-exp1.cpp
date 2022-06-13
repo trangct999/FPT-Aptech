ec5e49b6a0eeb600b84fe02a67f68642c15cd686
trangct999 <caotientrang@gmail.com>
2022-06-13 18:11:43 +0700

    Create SS6-exp1.cpp

---
+++ SS6-exp1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+	
+	int a = 10;
+	float b = 24.67882345;
+	char ch = 'A';
+	
+	printf ("\nInteger data =%d", a);
+	printf ("\nFload data =%f", b);
+	printf ("\nCharacter = %c", ch);
+	printf ("\nThis prints the string");
+	printf ("%s", "\nThis also prints a string");
+	
+	
+	
+	
+}
